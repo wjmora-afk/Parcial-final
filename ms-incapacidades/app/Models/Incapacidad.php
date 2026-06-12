49dcdc619bbf5f8309eda416d47924fe4bbd52b7
Nicolas <jnjimenez@uniboyaca.edu.co>
2026-06-11 22:22:39 -0500

    creación models

---
+++ ms-incapacidades/app/Models/Incapacidad.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Incapacidad extends Model
+{
+    protected $table = 'incapacidades';
+
+    public $timestamps = true;
+
+    protected $fillable = [
+        'empleado_id',
+        'fecha_inicio',
+        'fecha_fin',
+        'tipo',
+        'diagnostico_general',
+        'entidad_medica',
+        'observaciones',
+        'dias_incapacidad',
+        'estado'
+    ];
+}
